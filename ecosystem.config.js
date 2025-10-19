0e72b10ff54277a5617cf16f8d9937a4f2ff3590
romanmelnyk <romanmmmelnyk.uk@gmail.com>
2025-10-19 23:34:48 +0100

    Fix ecosystem.config.js to use port 7300 instead of 3000

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
     max_memory_restart: '1G',
     env: {
       NODE_ENV: 'production',
-      PORT: 3000
+      PORT: 7300
     },
     error_file: './logs/error.log',
     out_file: './logs/out.log',
